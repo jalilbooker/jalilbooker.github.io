069368a5ac99b5834a891acb78f106b62f42e8bb
jalilbooker <greatejalilbooker@gmail.com>
2022-09-18 18:18:28 -0700

    fix: minor

--- shared/components/loader.tsx
+++ shared/components/loader.tsx
@@ -4,7 +4,7 @@ export default function Loader(): JSX.Element {
       <div className="flex justify-center items-center h-screen bg-blue transition-all">
         <hr className="transition-all animate-changewidth duration-1000 absolute left-0 top-1/2" />
         <img
-          className="animate-bounce transition-all"
+          className="animate-bounce transition-all w-24"
           src="/images/logo-light.svg"
           alt="Jalil Logo"
         />
